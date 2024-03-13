a0b38d63072b2dfc3528ed9e33690a1b8d239124
archanasingh11 <archanasingh11702@gmail.com>
2024-03-13 12:41:25 +0530

    fourth-commit

--- Back-end/index.js
+++ Back-end/index.js
@@ -2,7 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 
-const app = express();
+const app = express(); //intializing express
 dotenv.config()
 
 //Schema
